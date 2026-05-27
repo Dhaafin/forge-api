58b610a0989ff182b9884999c58af4693e8da99c
Dhaafin <dhaafinm@gmail.com>
2026-05-27 13:48:55 +0700

    feat : fix a lot of typos

--- app/models/exercise.py
+++ app/models/exercise.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
 from app.core.database import Base
 
-class Excercise(Base):
+class Exercise(Base):
     __tablename__ = "exercises"
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
