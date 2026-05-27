aaa48a904683e3910fd60e0298fbef5c9ba765f0
Dhaafin <dhaafinm@gmail.com>
2026-05-27 14:04:52 +0700

    feat: add workouts router and update API description

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from fastapi import FastAPI
-from app.api.v1.endpoints import auth
+from app.api.v1.endpoints import auth, workouts 
 
 app = FastAPI(
     title="Forge Gym API",
-    description="Backend engine for Forge",
+    description="Backend engine for Forge Gym Tracker Platform",
     version="1.0.0",
 )
 
+# Routers
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Authentication"])
+app.include_router(workouts.router, prefix="/api/v1/workouts", tags=["Workouts Core"]) 
 
 @app.get("/")
 def read_root():
-    return{
+    return {
         "status": "online",
         "message": "Welcome to Forge API",
     }
