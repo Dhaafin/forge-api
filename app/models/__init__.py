eec0b12387505fe682046b717d034b03cae5e57a
Dhaafin <dhaafinm@gmail.com>
2026-07-15 10:29:23 +0700

    feat: register RefreshToken in models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.user import User
 from app.models.invite import InviteToken
+from app.models.refresh_token import RefreshToken
 from app.models.exercise import Exercise
 from app.models.workout_session import WorkoutSession
 from app.models.workout_set import WorkoutSet
